3a2b45dea4b25d4f3c5c81442a8c7d1de4d1db89
Marta Mordalska <s29953@pjwstk.edu.pl>
2026-05-09 23:43:39 +0200

    Initial commit

---
+++ APBD-PJATK-Cw3-s29953/Models/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD_PJATK_Cw3_s29953.Models;
+
+public class Reservation : IValidatableObject
+{
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    [Required]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required]
+    public string Topic { get; set; } = string.Empty;
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    [Required]
+    public string Status { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "EndTime must be later than StartTime",
+                new[] { nameof(EndTime) });
+        }
+    }
+
+}
